6a1d8729a60e6e47f6c8a19d4e33fce4d1c180c3
Calubag <camssycalubag@gmail.com>
2024-09-24 23:23:30 +0700

    add #1displayhelloworld

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { NgModule } from "@angular/core";
+import { CommonModule, CurrencyPipe } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+import { DisplayhelloworldComponent} from "./components/displayhelloworld/displayhelloworld.component";
+
+const routes: Routes = [
+  {path: '',  component:DisplayhelloworldComponent},
+
+
+
+
+
+
+
+
+]
+
+@NgModule ({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes, {enableTracing: true}),
+    FormsModule,
+  ],
+
+  declarations: [
+    AppComponent,
+    DisplayhelloworldComponent,
+
+
+
+  ],
+
+  providers: [
+
+  ],
+
+  bootstrap: [
+    AppComponent
+  ]
+})
+
+export class AppModule {}
