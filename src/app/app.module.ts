28d84163163608e64d3d3fb1b2ad6934b122474d
Calubag <camssycalubag@gmail.com>
2024-09-25 14:05:16 +0700

    add #16 SimpleLogin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import {WordreverserComponent} from "./components/wordreverser/wordreverser.comp
 import {ShowdateComponent} from "./components/showdate/showdate.component";
 import {ShowUsernameComponent} from "./components/showusername/showusername.component";
 import {MultiplicationtableComponent} from "./components/multiplicationtable/multiplicationtable.component";
+import {SimpleloginComponent} from "./components/simplelogin/simplelogin.component";
 
 
 const routes: Routes = [
@@ -38,6 +39,8 @@ const routes: Routes = [
   {path: 'showdate',  component: ShowdateComponent},
   {path: 'showusername',  component: ShowUsernameComponent},
   {path: 'multiplicationtable',  component: MultiplicationtableComponent},
+  {path: 'simplelogin',  component: SimpleloginComponent},
+
 
 
 
@@ -74,6 +77,7 @@ const routes: Routes = [
     ShowdateComponent,
     ShowUsernameComponent,
     MultiplicationtableComponent,
+    SimpleloginComponent,
 
 
   ],
---
+++ src/app/components/simplelogin/simplelogin.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simplelogin',
+  templateUrl: './simplelogin.component.html',
+  styleUrl: './simplelogin.component.css'
+})
+export class SimpleloginComponent {
+  email: string = '';
+  password: string = '';
+  message: string = '';
+  login() {
+    if (this.email && this.password) {
+      this.message = `Logged in successfully!`;
+    } else {
+      this.message = 'Please enter both email and password.';
+    }
+  }
+}
