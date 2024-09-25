d28ab46d5b201e02c94068134fe60c63c9f800bb
Calubag <camssycalubag@gmail.com>
2024-09-25 14:23:10 +0700

    add #17 FarenheitToCelcius

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import {ShowdateComponent} from "./components/showdate/showdate.component";
 import {ShowUsernameComponent} from "./components/showusername/showusername.component";
 import {MultiplicationtableComponent} from "./components/multiplicationtable/multiplicationtable.component";
 import {SimpleloginComponent} from "./components/simplelogin/simplelogin.component";
+import {FahrenheittocelsiusComponent} from "./components/fahrenheittocelsius/fahrenheittocelsius.component";
 
 
 const routes: Routes = [
@@ -40,6 +41,7 @@ const routes: Routes = [
   {path: 'showusername',  component: ShowUsernameComponent},
   {path: 'multiplicationtable',  component: MultiplicationtableComponent},
   {path: 'simplelogin',  component: SimpleloginComponent},
+  {path: 'fahrenheittocelsius',  component: FahrenheittocelsiusComponent},
 
 
 
@@ -78,6 +80,7 @@ const routes: Routes = [
     ShowUsernameComponent,
     MultiplicationtableComponent,
     SimpleloginComponent,
+    FahrenheittocelsiusComponent,
 
 
   ],
